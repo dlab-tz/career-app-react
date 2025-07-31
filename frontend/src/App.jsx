779c9b1e27e79a9c57161950716ddfd8765c0443
norbert <norbyzhojohn@gmail.com>
2025-07-31 12:56:34 +0300

    Fixed import to UserForm, now both components visible

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import UserSample from "./components/UserSample";
+import UserForm from "./components/UserForm";
 import ProfessionalField from "./components/ProfessionalField";
 
 function App() {
   return (
     <div>
-      <h2>User Sample Component</h2>
-      <UserSample />
+      <h2>User Form Component</h2>
+      <UserForm />
 
       <h2>Professional Field Component</h2>
       <ProfessionalField />
