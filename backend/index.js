c1f4165ad96a14cc54dfabea0e5fa35581a2a2cd
sangi-01 <jacksonchacha619@gmail.com>
2025-08-15 11:21:54 +0300

    Created sequelize file

--- backend/index.js
+++ backend/index.js
@@ -24,29 +24,7 @@ const pool = new Pool({
     }
 });
 
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'postgres',
-    port: process.env.DB_PORT,
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
 
-// Test Sequelize connection
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log(' Sequelize connected to DB');
-            } catch (error) {
-                console.error(' Sequelize connection error:', error);
-            }
-        })();
 
 
 module.exports = pool;
