ec6bd49f9d0b8bbe1433775eb1426760c6f9d979
Joseph H Samson <josephhsamson08@gmail.com>
2025-08-25 13:12:15 +0300

    Restrict access to users endpoint like (delete, edit, list) to only admin

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const usersController = require('../controllers/users.controller');
-
+const verifyToken = require("../middleware/auth");
 const router = Router();
-router.get('/', usersController.getAllUsers);
+
+router.get('/',verifyToken, usersController.getAllUsers);
 router.post('/', usersController.createUser);
-router.get('/:id', usersController.getUserById);
-router.put('/:id', usersController.updateUser);
-router.delete('/:id', usersController.deleteUser);
+router.get('/:id',verifyToken, usersController.getUserById);
+router.put('/:id',verifyToken, usersController.updateUser);
+router.delete('/:id', verifyToken,usersController.deleteUser);
+
 module.exports = router;
