724660edc652f260c9ce98d57a3a71ac285c6275
khadijamahanga <khadijamahanga@gmail.com>
2025-09-02 09:58:48 +0300

    remove merge error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,12 @@ import UserForm from "./components/UserForm";
 import AdminLogin from "./components/AdminLogin";
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      
-      <h2>User Form Component</h2>
-      <UserForm />
-
-     
-    </div>
-=======
     <Router>
       <Routes>
         <Route path="/" element={<UserForm />} />
         <Route path="/admin-login" element={<AdminLogin />} />
       </Routes>
     </Router>
->>>>>>> 3a1fb43b3623d50a991ea71987e701580533dea6
   );
 }
 
